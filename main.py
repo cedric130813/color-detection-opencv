af7ccea752133faa5d6e942145793b027d402d84
Ian Cedric Io <82087099+cedric130813@users.noreply.github.com>
2021-09-29 23:01:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
 import numpy as np
 import cv2
 
+# for built in webcam
 cap = cv2.VideoCapture(0)
-
+# for external webcam
+# cap = cv2.VideoCapture(1)
 while True:
     ret, frame = cap.read()
     width = int(cap.get(3))
     height = int(cap.get(4))
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_color_1 = np.array([0, 70, 50])
-    upper_color_1 = np.array([10, 255, 255])
-    lower_color_2 = np.array([170, 70, 50])
-    upper_color_2 = np.array([180, 255, 255])
+    red_lower_color_1 = np.array([0, 70, 50])
+    red_upper_color_1 = np.array([10, 255, 255])
+    red_lower_color_2 = np.array([170, 70, 50])
+    red_upper_color_2 = np.array([180, 255, 255])
 
-    mask1 = cv2.inRange(hsv, lower_color_1, upper_color_1)
-    mask2 = cv2.inRange(hsv, lower_color_2, upper_color_2)
+    red_mask_1 = cv2.inRange(hsv, red_lower_color_1, red_upper_color_1)
+    red_mask_2 = cv2.inRange(hsv, red_lower_color_2, red_upper_color_2)
 
-    color_boundaries = {
+    red_color_boundaries = {
         "1": ([0, 70, 50], [10, 255, 255]),
         "2": ([170, 70, 50], [180, 255, 255]),
     }
-    for color_name, (lower, upper) in color_boundaries.items():
-        if ( mask1.any() or mask2.any() ) == True:
+
+    blue_lower_color_1 = np.array([100,150,0])
+    blue_upper_color_1 = np.array([140,255,255])
+    blue_mask_1 = cv2.inRange(hsv, blue_lower_color_1, blue_upper_color_1)
+
+    blue_color_boundaries = {
+        "1": ([100,150,0], [140,255,255])
+    }
+    for red_color_name, (lower, upper) in red_color_boundaries.items():
+        if red_mask_1.any() or red_mask_2.any():
             print("Red is present")
+    for blue_color_name, (lower, upper) in blue_color_boundaries.items():
+        if blue_mask_1.any():
+            print("Blue is present")
 
-    result = cv2.bitwise_and(frame, frame, mask=mask1|mask2)
+    # For red color
+    red_result = cv2.bitwise_and(frame, frame, mask=red_mask_1 | red_mask_2)
+    blue_result = cv2.bitwise_and(frame, frame, mask =blue_mask_1)
 
-    cv2.imshow('frame', result)
-    # cv2.imshow('mask', mask1|mask2)
+    cv2.imshow('frame', red_result|blue_result)
+    # cv2.imshow('mask', blue_mask_1)
 
     if cv2.waitKey(1) == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
